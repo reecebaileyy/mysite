af214fc99cd408ee142443358f5144d16f037240
Reece Bailey <80913719+UNKNOWNtxn@users.noreply.github.com>
2023-06-26 23:58:58 -0700

    BEGIN

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,75 @@
+'use client'
 
 import Link from 'next/link'
+import Image from 'next/image'
+import { useState, useRef, useEffect } from 'react'
+import { HiOutlineX } from 'react-icons/hi'
 
 export default function Home() {
+
+  const [isOpen, setIsOpen] = useState(false)
+  const [darkMode, setDarkMode] = useState(false);
+  const menuRef = useRef()
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsOpen(false)
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuRef, setIsOpen]);
+
   return (
-    <div className="min-h-screen bg-blue-500 text-gray-900">
-      <nav className="p-6 border-b border-gray-300">
-        <Link className="mr-6" href="/">
-          Home
-        </Link>
-        <Link className="mr-6" href="/projects">
-          Projects
-        </Link>
-        <Link className="mr-6" href="/about">
-          About
-        </Link>
-        <Link className="mr-6" href="/contact">
-          Contact
-        </Link>
-      </nav>
-      <main className="p-6">
-        <h1 className="text-2xl mb-6">Welcome to my portfolio!</h1>
-        <p>This is a basic Next.js portfolio template with Tailwind CSS. Feel free to customize it to fit your needs.</p>
-      </main>
-    </div>
+    <>
+      <div className="min-h-screen bg-dark-blue">
+        <nav className="font-roboto font-light pt-10 px-5 md:px-10 flex justify-between items-center relative">
+          <Link href='/'>
+            <h1 className='text-xs text-bright-green'>RB the SWE</h1>
+          </Link>
+          <div className="xl:hidden 2xl:hidden 3xl:hidden">
+            <button onClick={() => setIsOpen(!isOpen)} className="flex items-center px-3 py-2 border rounded text-bright-green border-bright-green">
+              <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v15z" /></svg>
+            </button>
+            <div className={`fixed top-0 right-0 bottom-0 w-1/2 bg-dark-blue overflow-auto transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-200 ease-in-out`}
+              onClick={(e) => {
+                if (!menuRef.current.contains(e.target)) {
+                  setIsOpen(false)
+                }
+              }}>
+              <HiOutlineX className='text-light-gray absolute top-0 right-0 text-xl mt-5 mr-5 cursor-pointer' />
+              <ul ref={menuRef} className='items-center min-h-screen justify-center p-5 rounded-lg flex flex-col text-xl md:text-2xl'>
+                <li className='text-xs text-bright-green'>01.</li>
+                <li><Link className='text-sm text-light-gray hover:text-bright-green' href="#About">About</Link></li>
+                <li className='mt-5 text-xs text-bright-green'>02.</li>
+                <li><Link className='text-sm text-light-gray hover:text-bright-green' href="#Projects">Projects</Link></li>
+                <li className='mt-5 text-xs text-bright-green'>03.</li>
+                <li><Link className='text-sm text-light-gray hover:text-bright-green' href="#Contact">Contact</Link></li>
+                <li className='mt-5'><Link className='text-sm text-bright-green border-2 p-3 rounded-md border-solid border-bright-green' href="#Contact">Resume</Link></li>
+              </ul>
+            </div>
+          </div>
+          <div className='flex gap-2 items-center sm:hidden lg:hidden md:hidden'>
+            <ul className='flex md:hidden sm:hidden lg:hidden space-x-12 md:space-x-12 text-xl md:text-2xl'>
+              <li><Link className='text-xs text-light-gray hover:text-bright-green' href="#About"><span className='text-bright-green'>01.</span> About</Link></li>
+              <li><Link className='text-xs text-light-gray hover:text-bright-green' href="#Projects"><span className='text-bright-green'>02.</span> Projects</Link></li>
+              <li><Link className='text-xs text-light-gray hover:text-bright-green' href="#Contact"><span className='text-bright-green'>03.</span> Contact</Link></li>
+              <li><Link className='text-xs text-bright-green border-2 p-3 rounded-md border-solid border-bright-green' href="#Contact">Resume</Link></li>
+            </ul>
+          </div>
+        </nav>
+        <div className={`${isOpen ? 'blur-sm' : ''} flex flex-col items-center justify-center pt-24`}>
+          <div className="px-36 text-center">
+            <h3 className="font-roboto font-thin text-bright-green mb-6 text-xs md:text-sm lg:text-base">Hi, my name is</h3>
+            <h1 className="font-sans font-bold text-light-gray text-6xl sm:text-4xl md:text-4xl lg:text-5xl xl:text-6xl">Reece Bailey.</h1>
+            <h1 className="font-sans font-bold text-slate text-6xl sm:text-4xl md:text-4xl lg:text-5xl xl:text-6xl">I build things for the web.</h1>
+            <p className='font-light text-sm text-slate mt-5'>As a student at California State University Fullerton, I specialize in building exceptional, accessible, and human-centered digital experiences. With a focus on high performance full-stack applications, I delve into diverse fields like blockchain, machine learning, and database manipulation to create intelligent solutions.</p>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js"
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'dark-blue': '#0a192f',
+        'bright-green': '#64ffda',
+        'light-gray': '#ccd6f6',
+        'slate': '#8892b0',
       },
     },
+    fontSize: {
+      xs: '0.8rem',
+      sm: '1rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+      '6xl': '4.815rem',
+    },
+    screens: {
+      'sm': { 'min': '320px', 'max': '639px' },     // Phones (portrait)
+      'md': { 'min': '640px', 'max': '767px' },     // Phones (landscape) / Tablets (portrait)
+      'lg': { 'min': '768px', 'max': '1023px' },    // Tablets (landscape) / Laptops
+      'xl': { 'min': '1024px', 'max': '1279px' },   // Desktops (sm)
+      '2xl': { 'min': '1280px', 'max': '1535px' },  // Desktops (md)
+      '3xl': { 'min': '1536px', 'max': '10000px' },  // Desktops (lg)
+      'nv': { 'min': '640', 'max': '1536' }                   // Wide Boi (lg)
+    },
+    fontFamily: {
+      roboto: ['"Roboto"', 'sans-serif'],
+      sans: ['"Sans"', 'sans-serif'],
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ]
 }
